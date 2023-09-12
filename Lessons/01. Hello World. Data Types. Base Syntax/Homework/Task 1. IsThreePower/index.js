1ec454a2cacef25edee102adf4c25e9ac531c8e5
AmyArgotti <119971666+AmyArgotti@users.noreply.github.com>
2023-09-12 22:46:31 +0300

    Update index.js

--- Lessons/01. Hello World. Data Types. Base Syntax/Homework/Task 1. IsThreePower/index.js
+++ Lessons/01. Hello World. Data Types. Base Syntax/Homework/Task 1. IsThreePower/index.js
@@ -1,12 +1,13 @@
-const isThreePower = (num) => {
-
- if (num < 1) {
-    return false;
+function isThreePower(num) {
+  if (num === 1) {
+    return true;
   }
+  
+  let power = Math.log(num) / Math.log(3);
+  
+  return Number.isInteger(power);
+}
 
-  while (num % 3 === 0) {
-    num /= 3;
-  }
-
-  return num === 1;
-};
+console.log(isThreePower(1));    // true (3^0)
+console.log(isThreePower(2));    // false
+console.log(isThreePower(27));   // true (3^3
